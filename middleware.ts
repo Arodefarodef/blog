82df11e7adaf8a4b00f02f278010e6c9d17e8bef
Arodefarodef <umudefedora@gmail.com>
2024-07-16 09:07:56 -0700

    third

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const protectedRoute = createRouteMatcher(["/admin"]);
+const protectedRoute = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware((auth, req: any) => {
   if (protectedRoute(req)) auth().protect();
